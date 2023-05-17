f528edd002084443913ac309c99ede27b1a36ead
Awen <sean8922@sina.com>
2023-05-17 10:25:43 +0800

    git done table

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Table, { TableColumn } from './Table';
+interface TableData {
+  id: number;
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const data: TableData[] = [];
+for (let i = 0; i < 100; i++) {
+  data.push({
+    id: i,
+    name: `Edward ${i}`,
+    age: i + 1,
+    gender: `male. ${i}`,
+  });
+}
+
+const columns: TableColumn<TableData>[] = [
+  { key: 'id', title: 'ID', fixed: 'left', width: 100 },
+  { key: 'name', title: 'Name', fixed: 'left', width: 100 },
+  { key: 'age', title: 'Age', sortable: true },
+  { title: 'Column1', key: 'name' },
+  { title: 'Column 2', key: 'name' },
+  { title: 'Column 3', key: 'name' },
+  { title: 'Column 4', key: 'name' },
+  { title: 'Column 5', key: 'name' },
+  { title: 'Column 6', key: 'name' },
+  { title: 'Column 7', key: 'name' },
+  { title: 'Column 8', key: 'name', width: 100 },
+  { key: 'gender', title: 'Gender', fixed: 'right', width: 100 },
+];
+
+type TableSize = "middle" | "small" | "large";
 
 function App() {
+  const [tableSize, setTableSize] = useState<TableSize>('middle')
+
+  const handleTableSize = (size: TableSize) => {
+    setTableSize(size)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: '0 20px'}}>
+      <div style={{ margin: '20px 0' }}>
+        <button onClick={() => handleTableSize('small')}>small</button>
+        <button onClick={() => handleTableSize('middle')}>middle</button>
+        <button onClick={() => handleTableSize('large')}>large</button>
+      </div>
+      <Table
+        data={data}
+        columns={columns}
+        defaultSortColumn="age"
+        defaultSortDirection="asc"
+        pageSize={10}
+        scroll={{ x: 1500, y: 150 }}
+        size={tableSize}
+      />
     </div>
   );
 }
